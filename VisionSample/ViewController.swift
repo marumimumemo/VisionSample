f5ef0a5bd72d914062506015f9d0089af02d547f
marumimumemo <sagaminumber3@yahoo.co.jp>
2020-03-21 09:34:24 +0900

    Initial Commit

---
+++ VisionSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VisionSample
+//
+//  Created by 丸本聡 on 2020/03/21.
+//  Copyright © 2020 丸本聡. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
